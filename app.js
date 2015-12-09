ae38b6f91e5908378f78088b71141966f82985e4
Steven Cassidy <steven@mumsnet.com>
2015-12-09 10:58:22 -0500

    Add some examples

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var app = angular.module('steven', []);
 
 app.controller('MainController', function($scope, $http) {
-  $scope.data = [1,2,3,4,5];
+
+  $scope.removeSomething = function() {
+    $scope.sampleArray.pop();
+  };
+
+  $scope.addSomething = function() {
+
+    var max = Math.max(0, Math.max.apply(null, $scope.sampleArray));
+    $scope.sampleArray.push(max+1);
+  };
+
+  $scope.sampleArray = [1,2,3,4,5];
+
+  // Now let's load some stuff from reddit
+
   $http({
     method: 'GET',
     url: 'http://api.reddit.com'
